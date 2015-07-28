e86631e0f6b69bbee0503c0dd53332960291260f
Jeremy Nelson <jermnelson.gmail.com>
2015-07-28 15:51:45 -0600

    updated development

---
+++ static/music.js
@@ -0,0 +1,40 @@
+$("#new-music-recording").submit(function(event) {
+  var self = $("#new-music-recording");
+  var inputs = self.find("div").find(".form-control");
+  data = {};
+  console.log("In new music recording length inputs="+inputs.length);
+  event.preventDefault();
+  for(i=0; i < inputs.length; i++) {
+    var elem = $(inputs[i]);
+    switch(elem.prop("tagName")) {
+        case "INPUT":
+            switch(elem.attr('type')) {
+               case "file":
+                 console.log("Elem is a file");
+                 break;
+
+               default:
+                   data[elem.attr("name")] = elem.val()
+
+            }
+            break;
+ 
+        case "SELECT":
+            data[elem.attr("name")] = elem.select().val()
+            console.log();
+            break;
+
+        default:
+            console.log(elem.attr('type'));
+
+
+    }
+    console.log(data);
+    $.post("/new",
+           data=data,
+           function(err, data) {
+
+
+           });
+  }
+});
