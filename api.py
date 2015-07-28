c2bc2609a3560ebe48536f90e253481e4fc0c00a
Ximenabuller <ximena.bullermachado@coloradocollege.edu>
2015-07-28 15:37:50 -0600

    Added tests and semantic server submodule

--- api.py
+++ api.py
@@ -2,6 +2,9 @@ __author__="Ximena Buller Machado, Jeremy Nelson"
 __license__="GNU AFFEROv3"
 
 import falcon
+import rdflib
+
+app = falcon.API()
 
 class AudioObject(object):
 
@@ -9,11 +12,23 @@ class AudioObject(object):
         resp.body = '{"message": "AudioObject"}'
         resp.status = falcon.HTTP_200
 
+    def on_post(self, req, resp):
+        resp.body = '{"message": "Created AudioObject"}'
+        resp.status = falcon.HTTP_201
+
+app.add_route("/AudioObject", AudioObject())
+
 class MusicPlaylist(object):
 
     def on_get(self, req, resp):
         resp.body = '{"message": "MusicPlaylist"}'
         resp.status = falcon.HTTP_200
+
+    def on_post(self, req, resp):
+        resp.body = '{"message": "Created MusicPlaylist"}'
+        resp.status = falcon.HTTP_201
+
+app.add_route("/MusicPlaylist", MusicPlaylist())
 
 class MusicRecording (object):
 
@@ -21,17 +36,35 @@ class MusicRecording (object):
         resp.body = '{"message": "MusicRecording"}'
         resp.status = falcon.HTTP_200
 
+    def on_post(self, req, resp):
+        resp.body = '{"message": "Created MusicRecording"}'
+        resp.status = falcon.HTTP_201
+
+app.add_route("/MusicRecording", MusicRecording())
+
 class MusicGroup(object):
     
      def on_get(self, req, resp):
         resp.body = '{"message": "MusicGroup"}'
         resp.status = falcon.HTTP_200
 
+     def on_post(self, req, resp):
+        resp.body = '{"message": "Created MusicGroup"}'
+        resp.status = falcon.HTTP_201
+
+app.add_route("/MusicGroup", MusicGroup())
+
 class Person(object):
 
     def on_get(self, req, resp):
         resp.body = '{"message": "Person"}'
         resp.status = falcon.HTTP_200
+
+    def on_post(self, req, resp):
+        resp.body = '{"message": "Created Person"}'
+        resp.status = falcon.HTTP_201
+
+app.add_route("/Person", Person()) 
         
         
 
